60ab36d40539f1709db02c4d8ae69b6c5c90815b
kamesh <kamesh@gmail.com>
2017-12-08 11:47:58 +0530

    adding new file for demo

---
+++ demo.groovy
@@ -0,0 +1,31 @@
+pipeline{
+    agent any
+    stages{        
+        stage('start nginx'){
+            steps{
+                bat '''cd D:\\Tools\\nginx-1.12.2
+                    start nginx'''
+            }
+        }
+        stage('list nginx pid'){
+            steps{
+                bat '''tasklist /fi "imagename eq nginx.exe'''
+            }
+        }
+        stage('approve stage'){
+            steps{
+                input 'proceed to stop job'
+            }
+        }
+        stage('kill nginx process'){
+            steps{
+                bat '''taskkill /F /fi "imagename eq nginx.exe'''
+            }
+        }
+        stage('list nginx pid after kill'){
+            steps{
+                bat '''tasklist /fi "imagename eq nginx.exe'''
+            }
+        }
+    }
+}
